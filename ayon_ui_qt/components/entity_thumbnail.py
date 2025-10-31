ce012e7c742c172dcb45f9619836ad334cd0696b
philippe-ynput <philippe@ynput.io>
2025-10-31 15:59:55 +0100

    refactor(entity_thumbnail): simplify component initialization and remove unused styles
    - Replace explicit QtWidgets/QtGui imports with specific class imports
    - Change __init__ signature from positional parent parameter to **kwargs for flexibility
    - Remove commented-out stylesheet code
    - Simplify QSizePolicy instantiation by removing redundant module prefixes
    
    Signed-off-by: philippe-ynput <philippe@ynput.io>

--- ayon_ui_qt/components/entity_thumbnail.py
+++ ayon_ui_qt/components/entity_thumbnail.py
@@ -1,22 +1,10 @@
-from qtpy import QtCore, QtGui, QtWidgets
+from qtpy.QtWidgets import QPushButton, QSizePolicy
+from qtpy.QtGui import QIcon
 
 
-class AYEntityThumbnail(QtWidgets.QPushButton):
-    def __init__(self, parent):
-        self._icon = QtGui.QIcon()
-        super().__init__(self._icon, "", parent=parent)
-        self.setSizePolicy(
-            QtWidgets.QSizePolicy.Policy.Fixed,
-            QtWidgets.QSizePolicy.Policy.Fixed,
-        )
+class AYEntityThumbnail(QPushButton):
+    def __init__(self, **kwargs):
+        self._icon = QIcon()
+        super().__init__(self._icon, "", **kwargs)
+        self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         self.setFixedSize(85, 48)
-
-        # self.setStyleSheet(
-        #     """AYEntityThumbnail {
-        #         background-color: #000000;
-        #         border: 2px #8B9198;
-        #         border-style: solid;
-        #         border-radius: 4px;
-        #     }
-        #     """
-        # )
